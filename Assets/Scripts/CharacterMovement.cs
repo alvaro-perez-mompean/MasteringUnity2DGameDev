faffb2c0920fea91773adb3ed38755187bd8916e
apmomp <apmomp@gmail.com>
2016-02-01 21:42:03 +0100

    Create player's run animation and move sprite inside Gameobject

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -5,17 +5,24 @@ public class CharacterMovement : MonoBehaviour {
     private Rigidbody2D playerRigidbody2D;
     private float movePlayerVector;
     private bool facingRight;
+    private GameObject characterSprite;
+
+    private Animator anim;
 
     public float speed = 4.0f;
 
 	// Use this for initialization
 	void Awake () {
         playerRigidbody2D = GetComponent<Rigidbody2D>();
+
+        characterSprite = transform.Find("PlayerSprite").gameObject;
+        anim = characterSprite.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         movePlayerVector = Input.GetAxis("Horizontal");
+        anim.SetFloat("speed", Mathf.Abs(movePlayerVector));
 
         playerRigidbody2D.velocity = new Vector2(movePlayerVector * speed, playerRigidbody2D.velocity.y);
 
@@ -32,8 +39,8 @@ public class CharacterMovement : MonoBehaviour {
     {
         facingRight = !facingRight;
 
-        Vector3 theScale = transform.localScale;
+        Vector3 theScale = characterSprite.transform.localScale;
         theScale.x *= -1;
-        transform.localScale = theScale;
+        characterSprite.transform.localScale = theScale;
     }
 }
