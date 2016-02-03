d9257086476287beea477beb7dc94232ff6f5013
apmomp <apmomp@gmail.com>
2016-02-03 19:41:21 +0100

    Implement NavigationManager

---
+++ Assets/Scripts/NavigationManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class NavigationManager {
+    public static Dictionary<string, string> RouteInformation = new Dictionary<string, string>()
+    {
+        { "World", "The big bad world" },
+        { "Cave", "The deep dark cave" },
+        { "Town", "Main town" }
+    };
+
+    public static string GetRouteInfo(string destination)
+    {
+        return RouteInformation.ContainsKey(destination) ? RouteInformation[destination] : null;
+    }
+
+    public static bool CanNavigate(string destination)
+    {
+        return true;
+    }
+
+    public static bool NavigateTo(string destination)
+    {
+        if (CanNavigate(destination))
+        {
+            SceneManager.LoadScene(destination);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
--- Assets/Scripts/NavigationPrompt.cs
+++ Assets/Scripts/NavigationPrompt.cs
@@ -9,7 +9,11 @@ public class NavigationPrompt : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D col)
     {
-        showDialog = true;
+        if (NavigationManager.CanNavigate(destination))
+        {
+            showDialog = true;
+        }
+        
     }
 
     void OnCollisionExit2D(Collision2D col)
@@ -23,12 +27,11 @@ public class NavigationPrompt : MonoBehaviour {
         {
             GUI.BeginGroup(new Rect(Screen.width / 2 - 150, 50, 300, 250));
             GUI.Box(new Rect(0, 0, 300, 250), "");
-            GUI.Label(new Rect(15, 10, 300, 68), "Do you want to travel to " + destination + "?");
+            GUI.Label(new Rect(15, 10, 300, 68), "Do you want to travel to " + NavigationManager.GetRouteInfo(destination) + "?");
             if (GUI.Button(new Rect(55, 100,180,40), "Travel"))
             {
                 showDialog = false;
-                // TODO
-                //SceneManager.LoadScene(1)
+                NavigationManager.NavigateTo(destination);
             }
 
             if (GUI.Button(new Rect(55, 150, 180, 40), "Stay"))
