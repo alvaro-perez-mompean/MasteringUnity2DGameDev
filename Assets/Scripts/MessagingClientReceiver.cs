d54c3f82cad4247361807978f1344999373ef193
apmomp <apmomp@gmail.com>
2016-02-03 20:54:19 +0100

    Create messaging manager and fix Greybeard animation

---
+++ Assets/Scripts/MessagingClientReceiver.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MessagingClientReceiver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MessagingManager.Instance.Subscribe(ThePlayerIsTryingToLeave);
+	}
+	
+	void ThePlayerIsTryingToLeave () {
+        Debug.Log("Don't leave me!! - " + tag.ToString());
+	}
+}
---
+++ Assets/Scripts/MessagingManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class MessagingManager : MonoBehaviour {
+    public static MessagingManager Instance
+    {
+        get;
+        private set;
+    }
+
+    private List<Action> subscribers = new List<Action>();
+
+    void Awake()
+    {
+        Debug.Log("Messaging manager started");
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        Instance = this;
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void Subscribe(Action subscriber)
+    {
+        subscribers.Add(subscriber);
+    }
+
+    public void Unsubscribe(Action subscriber)
+    {
+        subscribers.Remove(subscriber);
+    }
+
+    public void ClearAllSubscribers()
+    {
+        subscribers.Clear();
+    }
+
+    public void Broadcast()
+    {
+        Debug.Log("Broadcast requested, Nº of subscribers: " + subscribers.Count);
+
+        foreach(var subscriber in subscribers)
+        {
+            subscriber();
+        }
+    }
+}
